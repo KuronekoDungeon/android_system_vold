be7a50748b5810ec61f5b03e0c44519cd6c39900
maz-1 <ohmygod19993@gmail.com>
2016-07-08 10:41:42 +0800

    add Hfsplus support & some fix

--- Android.mk
+++ Android.mk
@@ -12,8 +12,9 @@ common_src_files := \
 	fs/Ext4.cpp \
 	fs/F2fs.cpp \
 	fs/Ntfs.cpp \
+	fs/Hfsplus.cpp \
 	fs/Vfat.cpp \
-	fs/fsexist.c \
+	fsexist.c \
 	Loop.cpp \
 	Devmapper.cpp \
 	ResponseCode.cpp \
